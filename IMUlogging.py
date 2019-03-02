8afcb66184d4ec3a0ef77644aa3cd1f093f66861
AbhikChowdhury6 <abhik.ch6@gmail.com>
2019-03-02 20:41:38 +0000

    added second IMU logging:

--- IMUlogging.py
+++ IMUlogging.py
@@ -8,6 +8,7 @@ import sys
 
 #import all of the devices on the bus for now
 from i2cMPU60500 import MPU60500
+from i2cMPU60501 import MPU60501
 bus = smbus.SMBus(1)
 
 #defince product name
@@ -17,6 +18,7 @@ DATA_PATH = = sys.argv[1]
 
 # extanciate and configure all of the objects
 mpu60500 = MPU60500(bus, DATA_PATH)
+mpu60501 = MPU60501(bus, DATA_PATH)
 
 
 print time.strftime('%d-%m-%Y-%H-%M-%S', time.localtime())
@@ -27,6 +29,9 @@ while True:
 
   if (divider % mpu60500.REFRESH_RATE == 0):
       mpu60500.log(bus)
+
+  if (divider % mpu60501.REFRESH_RATE == 0):
+      mpu60501.log(bus)
 
   divider = divider + 1
   time.sleep(0.01 - ((time.time() - starttime) % 0.01))
