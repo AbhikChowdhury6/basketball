eed2445439fa13f049b73d56b77a1f598069540e
AbhikChowdhury6 <abhik.ch6@gmail.com>
2019-03-03 17:15:49 +0000

    added test code for BNO055

--- 3plotCSV.py
+++ 3plotCSV.py
@@ -1,13 +1,23 @@
 import matplotlib.pyplot as plt
 import sys
 
-with open(sys.argv[1], 'r') as myfile:
+sensorType = sys.argv[1]
+
+with open(sensorType, 'r') as myfile:
     raw=myfile.read().replace('\n', '')
 s = raw.split(",")
 s.pop()
 print("number of data poits: " + str(len(s)))
 
 #print s
+plt.plot(map(int,s))
+plt.ylabel('some numbers')
+plt.show()
+
+plt.plot(map(int,s))
+plt.ylabel('some numbers')
+plt.show()
+
 plt.plot(map(int,s))
 plt.ylabel('some numbers')
 plt.show()
